13f2338a9b83f78aab1b11a3eb649c41944a5104
Yury Mitsiuk <yriymitsuik@gmail.com>
2018-05-30 11:31:48 +0300

    Init commit

---
+++ src/main/java/guru/springframework/springmvccourse/SpringMvcCourseApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.springmvccourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCourseApplication.class, args);
+    }
+}
