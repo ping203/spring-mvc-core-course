b3b8c23ae40f8055f61312f6dc2b15d4624b578b
Yury Mitsiuk <yriymitsuik@gmail.com>
2018-05-31 12:51:27 +0300

    testing of IndexController

---
+++ src/test/java/guru/springframework/springmvccourse/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.springmvccourse.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by yriyMitsiuk on 31.05.2018.
+ */
+public class IndexControllerTest {
+
+    private MockMvc mockMvc;
+
+    private IndexController indexController;
+
+    @Before
+    public void setup() {
+        indexController = new IndexController();
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+}
